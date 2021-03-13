85dada615884b05726318bd2014a63fc0c4c92e7
Braxoia <71882015+Braxoia@users.noreply.github.com>
2021-03-13 02:02:21 +0100

    Update Pioche.java

--- Pioche.java
+++ Pioche.java
@@ -1,28 +1,39 @@
-package appli;
+package appli.jeu;
 
 import java.util.Collections;
-import java.util.Stack;
+import java.util.LinkedList;
 
 public class Pioche {
-    private Stack<Byte> pileDeCartes = new Stack<>();
+    // La pioche du joueur
+    private final LinkedList<Integer> pioche;
 
-    public int NombreCartesPioche(){
-        return pileDeCartes.size();
+    /**
+     * Constructeur
+     * @param carteMinimale la carte la plus petite qui sera dans la pile
+     * @param carteMaximale la carte la plus grande qui sera dans la pile
+     */
+    public Pioche(int carteMinimale, int carteMaximale){
+        pioche  = new LinkedList<>();
+        //Comme les cartes 1 et 60 sont déjà placées sur les piles, la pioche ne contient que les cartes de 2 à 59
+        for(int i = carteMinimale; i <= carteMaximale; ++i)
+            pioche.push(i);
+
+        Collections.shuffle(pioche);
     }
 
-    public byte piocher (){
-        return pileDeCartes.pop();
+    /**
+     * Récupère le nombre de cartes dans la pioche du joueur
+     * @return la taille de la pioche
+     */
+    public int getNombreCartes() {
+        return pioche.size();
     }
 
-    public Pioche(){
-        for(int i = 2; i <= 59; ++i)
-        {
-            pileDeCartes.push((byte)i);
-        }
-        Collections.shuffle(pileDeCartes);
-    }
-
-    public Stack<Byte> getPileDeCartes() {
-        return pileDeCartes;
+    /**
+     * Permet au joueur de piocher une carte dans la pioche
+     * @return la carte venant d'être piochée
+     */
+    public int piocher(){
+        return pioche.pop();
     }
 }
