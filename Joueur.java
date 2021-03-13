f57d177ece1d8a950e803105b6accf0b0d9ee2f2
Braxoia <71882015+Braxoia@users.noreply.github.com>
2021-03-13 02:01:04 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,102 +1,129 @@
-package appli;
+package appli.jeu;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Joueur extends Object{
+public class Joueur {
+    // Nom du joueur
     private final String nom;
-    private final Pile pile;
-    private final ArrayList<Byte> cartesEnMain;
+
+    // Les piles du joueur
+    private final Pile pileAscendante;
+    private final Pile pileDescendante;
+
+    // Liste des cartes en main
+    private final ArrayList<Integer> cartesEnMain;
+
+    // La pioche du joueur
     private final Pioche pioche;
 
-    Joueur(String nom)
+    // Constructeur par défaut
+    public Joueur(String nom)
     {
         this.nom = nom;
         cartesEnMain = new ArrayList<>();
-        pile = new Pile();
-        pioche = new Pioche();
-        piocher(6);
+
+        pileAscendante = new Pile(Constantes.CARTE_INITIALE_PILE_ASCENDANTE, TypePile.ASCENDANTE);
+        pileDescendante = new Pile(Constantes.CARTE_INITIALE_PILE_DESCENDANTE, TypePile.DESCENDANTE);
+
+        pioche = new Pioche(Constantes.CARTE_INITIALE_PILE_ASCENDANTE +1, Constantes.CARTE_INITIALE_PILE_DESCENDANTE -1);
+        piocher(Constantes.NB_CARTES_MAIN_INITIALES);
     }
 
-    public Pile getPile(){
-        return pile;
+    /**
+     * Récupère une des piles du joueur
+     * @param typePile le type de la pile à récupérer
+     * @return la pile du joueur désirée
+     */
+    public Pile getPile(TypePile typePile){
+        return typePile == TypePile.ASCENDANTE? pileAscendante : pileDescendante;
     }
 
-    public ArrayList<Byte> getCarteEnMain() {
+    /**
+     * Récupère les cartes en main du joueur
+     * @return La liste des cartes en main
+     */
+    public ArrayList<Integer> getCartesEnMain() {
         return cartesEnMain;
     }
 
-    public void piocher(int nbCartes)
+    /**
+     * Permet au joueur de piocher des cartes
+     * @param nombreCartes le nombre de cartes que l'on souhaite piocher
+     * @return le nombre de cartes que le joueur a pioché au final
+     * (n'est pas forcément égal à nombreCartes car la pioche peut s'épuiser avant que toutes les cartes aient été piochées)
+     */
+    public int piocher(int nombreCartes)
     {
-        byte cartePioche;
-        for(int i = 0; i < nbCartes; ++i) {
-            cartePioche = pioche.piocher();
-            cartesEnMain.add(cartePioche);
-        }
+        /*
+            S'il y a plus de cartes à piocher que de cartes dans la pioche, alors on pioche seulement toutes les cartes
+            Restantes dans la pioche
+        */
+        int nombreCartesPiochees = Math.min(nombreCartes, pioche.getNombreCartes());
+
+        for(int i = 0; i < nombreCartesPiochees; ++i)
+            cartesEnMain.add(pioche.piocher());
+
+        //On trie la main du joueur
+        Collections.sort(cartesEnMain);
+        return nombreCartesPiochees;
     }
 
-    @Override
-    public String toString()
+    /**
+     * Retourne une chaine de caractères présentant l'état du plateau et de la main du joueur
+     * @return la chaine de caractères mentionnée ci-dessus
+     */
+    public String informationsPlateau()
     {
-        StringBuilder affiche = new StringBuilder();  //NORD ^[01] v[60] (m6p52) SUD ^[01] v[60] (m6p52)
+        StringBuilder affiche = new StringBuilder();
 
-            affiche.append(nom).append(" ^[").append(String.format("%02d", getPile().getCarteSommetAscendante())).append("] v[").append(String.format("%02d", getPile().
-                    getCarteSommetDescendante())).append("] (m").append(cartesEnMain.size()).append("p").
-                    append(pioche.NombreCartesPioche()).append(")");
+        //On utilise le format %O2d afin d'ajouter un zéro à gauche si le nombre est un chiffre
+        affiche.append(nom)
+                .append(" ^[")
+                .append(String.format("%02d", pileAscendante.getCarteSommet()))
+                .append("] v[")
+                .append(String.format("%02d", pileDescendante.getCarteSommet()))
+                .append("] (m")
+                .append(cartesEnMain.size())
+                .append("p")
+                .append(pioche.getNombreCartes())
+                .append(")");
 
         return affiche.toString();
     }
 
-    public String cartesMain() {
+    /**
+     * Retourne une chaîne de caractères listant les cartes de la main du joueur
+     * @return la chaine de caractères mentionnée ci-dessus
+     */
+    public String informationsCartesMain() {
         StringBuilder affiche = new StringBuilder();
 
-        affiche.append("cartes ").append(nom).append(" { ");
-        Collections.sort(cartesEnMain);
-        for(byte s : cartesEnMain)
-            affiche.append(String.format("%02d", s)).append(" ");
+        affiche.append("cartes ")
+                .append(nom)
+                .append(" { ");
 
+        for(int i : cartesEnMain)
+            affiche.append(String.format("%02d", i))
+                    .append(" ");
 
         affiche.append("}");
         return affiche.toString();
     }
 
-    public boolean PeutJouerPartie (){
-        ArrayList<Byte> cartesJouablesAscendantes = new ArrayList<>();
-        ArrayList<Byte> cartesJouablesDescendantes = new ArrayList<>();
-
-        byte carteAscendante = pile.getCarteSommetAscendante(), carteDescendante = pile.getCarteSommetDescendante();
-
-        for(Byte carte : cartesEnMain) {
-            if ((carte > carteAscendante || carte + 10 == carteAscendante)) {
-                cartesJouablesAscendantes.add(carte);
-                carteAscendante = carte;
-            }
-
-        }
-
-        for(int i = cartesEnMain.size() - 1; i >= 0; i--)
-        {
-            byte carte = cartesEnMain.get(i);
-            if(carte < carteDescendante || carte - 10 == carteDescendante)
-            {
-                cartesJouablesDescendantes.add(carte);
-                carteDescendante = carte;
-            }
-        }
-
-        if(cartesJouablesDescendantes.size() >= 2 || cartesJouablesAscendantes.size() >= 2 ||
-                             (cartesJouablesDescendantes.size() == 1 && cartesJouablesAscendantes.size() == 1
-                                     && !cartesJouablesDescendantes.get(0).equals(cartesJouablesAscendantes.get(0))))
-                         return true;
-
-        // pile descendante 34
-        // 35 44
-        return false;
+    /**
+     * Retourne le nom du joueur
+     * @return le nom du joueur
+     */
+    public String toString() {
+        return nom;
     }
 
-    public boolean partieGagnee(Joueur joueur2){
-        return (pioche.getPileDeCartes().empty() && cartesEnMain.isEmpty()) || joueur2.partiePerdue();
+    /**
+     * Retourne le nombre de cartes dans la pioche du joueur
+     * @return le nombre de cartes dans la pioche du joueur
+     */
+    public int getTaillePioche() {
+        return pioche.getNombreCartes();
     }
-
-    public boolean partiePerdue(){ return !PeutJouerPartie(); }
 }
