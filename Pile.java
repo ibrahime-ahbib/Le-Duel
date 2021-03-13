077b4293b79e274cb13d6415c569ab676fd60939
Braxoia <71882015+Braxoia@users.noreply.github.com>
2021-03-13 02:01:52 +0100

    Update Pile.java

--- Pile.java
+++ Pile.java
@@ -1,34 +1,65 @@
-package appli;
+package appli.jeu;
 
 public class Pile {
-    private byte nbCartes;
-    private byte carteSommetAscendante;
-    private byte carteSommetDescendante;
+    /*
+        En théorie une pile dans "The Game" est constituée de toutes les cartes qui ont déjà été jouées dessus, mais
+        dans notre cas seule la carte du dessus suffit.
+    */
+    private int carteSommet;
 
-    public Pile(){
-        nbCartes = 58;
-        carteSommetDescendante = 60;
-        carteSommetAscendante = 1;
+    // Une pile est soit ascendante soit descendante.
+    private TypePile typePile;
+
+    // Constructeur par défaut
+    public Pile(int valeurInitiale, TypePile typePile) {
+        this.typePile = typePile;
+        carteSommet = valeurInitiale;
     }
 
-    public byte getCarteSommetAscendante(){
-        return carteSommetAscendante;
+    // Constructeur de copie
+    public Pile(Pile autre){
+        set(autre);
     }
 
-    public byte getCarteSommetDescendante(){
-        return carteSommetDescendante;
+    public void set(Pile autre) {
+        this.typePile = autre.typePile;
+        carteSommet = autre.carteSommet;
     }
 
-
-    public void setCarteSommetDescendante(byte CarteSommetDescendante) {
-        this.carteSommetDescendante = CarteSommetDescendante;
+    /**
+     * Renvoie la carte au sommet de la pile.
+     * @return la carte au sommet de la pile.
+     */
+    public int getCarteSommet() {
+        return carteSommet;
     }
 
-    public void setCarteSommetAscendante(byte CarteSommetAscendante) {
-        this.carteSommetAscendante = CarteSommetAscendante;
+    /**
+     * Change la carte du sommet de la pile
+     * @param nouvelleCarteSommet la carte qui va remplacer la carte actuelle sur le sommet.
+     */
+    public void setCarteSommet(int nouvelleCarteSommet) {
+        this.carteSommet = nouvelleCarteSommet;
     }
-    //MiseAJour de la pile
 
+    /**
+     * Vérifie si une carte est jouable sur la pile, en prenant en compte la règle +10/-10 si besoin
+     * @param carte la carte à vérifier
+     * @param pileAdverse si ce booléen est vrai alors on doit appliquer la règle +10/-10 (en fonction du type de la pile)
+     * @return true si la carte est jouable, false sinon
+     */
+    public boolean carteJouable(int carte, boolean pileAdverse){
+        if(typePile == TypePile.ASCENDANTE) {
+            if(pileAdverse)
+                return carte < carteSommet;
+            else
+                return carte > carteSommet || carte + Constantes.DIFFERENCE_CARTE_JOUABLE == carteSommet;
+        }
+        else {
+            if(pileAdverse)
+                return carte > carteSommet;
+            else
+                return carte < carteSommet || carte - Constantes.DIFFERENCE_CARTE_JOUABLE == carteSommet;
+        }
+    }
 }
-
-
